a2b2c04f129c96632b1fd80dbad0f730c0a022fc
linesynth-a11y <linesynth@gmail.com>
2026-09-23 12:20:06 +0700

    Initial commit

---
+++ app/src/main/java/com/sample/modifierdemo/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.sample.modifierdemo
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.font.FontWeight
+
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.sample.modifierdemo.ui.theme.ModifierDemoTheme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ModifierDemoTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    DemoScreen(Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+@Composable
+fun DemoScreen(modifier: Modifier = Modifier) {
+    val mymodifier = modifier
+        .border(width = 2.dp, Color.Black)
+        .padding(all = 10.dp)
+    Text(
+        "Hello Compose",
+        modifier = mymodifier,
+        fontSize = 40.sp,
+        fontWeight = FontWeight.Bold
+    )
+}
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ModifierDemoTheme {
+        DemoScreen()
+    }
+}
